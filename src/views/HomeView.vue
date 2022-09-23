625dc999759dfd0c444e6808f45ccf6127bad7f2
Yanek-K <yanek.dev@gmail.com>
2022-09-23 12:43:40 -0400

    Event Cards

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,55 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import EventCard from "@/components/EventCard.vue";
 
 export default {
   name: "HomeView",
   components: {
-    HelloWorld,
+    EventCard,
+  },
+  data() {
+    return {
+      events: [
+        {
+          id: 1,
+          title: "Track Day",
+          description: "Get some serious speed",
+          location: "Exotic Car Rentals",
+          date: "October 9th, 2022",
+          time: "12pm",
+        },
+        {
+          id: 2,
+          title: "Indoor Sky Diving",
+          description: "Wanna fall from the sky?",
+          location: "West-End",
+          date: "November 2022",
+          time: "12pm",
+        },
+        {
+          id: 3,
+          title: "Swinging in the Trees",
+          description: "Afraid of heights?",
+          location: "Forest",
+          date: "December 2022",
+          time: "12pm",
+        },
+      ],
+    };
   },
 };
 </script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
