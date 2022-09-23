3d255f1867c86173cea1a1a7ecaaf138b02cfab6
Yanek-K <yanek.dev@gmail.com>
2022-09-23 13:39:07 -0400

    Added db.json

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -16,32 +16,7 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 1,
-          title: "Track Day",
-          description: "Get some serious speed",
-          location: "Exotic Car Rentals",
-          date: "October 9th, 2022",
-          time: "12pm",
-        },
-        {
-          id: 2,
-          title: "Indoor Sky Diving",
-          description: "Wanna fall from the sky?",
-          location: "West-End",
-          date: "November 2022",
-          time: "12pm",
-        },
-        {
-          id: 3,
-          title: "Swinging in the Trees",
-          description: "Afraid of heights?",
-          location: "Forest",
-          date: "December 2022",
-          time: "12pm",
-        },
-      ],
+      events: null,
     };
   },
 };
