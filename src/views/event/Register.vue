095fb14ce2d982110571299285c8be1a14a4a348
Yanek-K <yanek.dev@gmail.com>
2022-09-27 13:15:12 -0400

    In-Component Route Guards Complete

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -8,10 +8,19 @@
 <script>
 export default {
   props: ["event"],
+  inject: ["GStore"],
   methods: {
     register() {
       // Call to API
       // If success, redirect to event details
+
+      this.GStore.flashMessage =
+        "You are successfully registed for " + this.event.title;
+
+      setTimeout(() => {
+        this.GStore.flashMessage = "";
+      }, 3000);
+
       this.$router.push({
         name: "EventDetails",
       });
